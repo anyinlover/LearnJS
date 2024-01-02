8abc5a8f8fd541e4490201722e60c8d849f37201
guhangsong <guhangsong@outlook.com>
2024-01-02 20:24:52 +0800

    update elquent js chap2

--- eloquent_javascript/chap2/chessboard.js
+++ eloquent_javascript/chap2/chessboard.js
@@ -1,12 +1,15 @@
 let board = "";
 let size = 8;
+let isBlank = true;
 for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
-        if ((i + j) % 2 == 0)
+        if (isBlank)
             board += " ";
         else
             board += "#";
+        isBlank = !isBlank;
     }
     board += "\n";
+    isBlank = !isBlank;
 }
 console.log(board);
